ca2fbbd9f8d0e68192c948d8249f82d4c1e400e2
KrasnovDenis <Elo.1972@yandex.ru>
2019-11-02 19:18:29 +0400

    Data access

---
+++ src/main/java/nc/Medas/Models/UserRepo.java
@@ -0,0 +1,6 @@
+package nc.Medas.Models;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepo extends CrudRepository<User,Long> {
+}
--- src/main/java/nc/Medas/controller/MainController.java
+++ src/main/java/nc/Medas/controller/MainController.java
@@ -1,16 +1,54 @@
 package nc.Medas.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import nc.Medas.Exceptions.FilmNotFoundException;
+import nc.Medas.Exceptions.IdNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("medas")
+@RequestMapping("/")
 public class MainController {
+    private List<Map<String, String>> list = new ArrayList<Map<String, String>>() {{
+        add(new HashMap<String, String>() {{
+            put("id", "1");
+            put("film", "Titanic");
+        }});
+        add(new HashMap<String, String>() {{
+            put("id", "2");
+            put("film", "Red");
+        }});
+        add(new HashMap<String, String>() {{
+            put("id", "3");
+            put("film", "1408");
+        }});
+    }};
+
     @GetMapping
-    public String list() {
-        return "indeеееx";
+    public List<Map<String, String>> list() {
+        return list;
     }
+
+    @GetMapping("{id}")
+    public Map<String, String> getFilmById(@PathVariable String id) throws IdNotFoundException {
+        return list.stream().
+                filter(list -> list.get("id").
+                        equals(id)).
+                findFirst().orElseThrow(IdNotFoundException::new);
+    }
+
+    @GetMapping("{film}")
+    public Map<String, String> getFilm (@PathVariable String film) throws FilmNotFoundException {
+        return list.stream().
+                filter(list -> list.get("film").
+                        equals(film)).
+                findFirst().orElseThrow(FilmNotFoundException::new);
+    }
+
+
+
 
 }
