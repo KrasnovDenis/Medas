ca7b96e72da98ba2eb5bca14b226381d4a1cb478
KrasnovDenis <Elo.1972@yandex.ru>
2019-11-02 12:11:16 +0400

    first launch

---
+++ src/main/java/nc/Medas/MedasApplication.java
@@ -0,0 +1,13 @@
+package nc.Medas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nc/Medas/MedasApplicationTests.java
@@ -0,0 +1,13 @@
+package nc.Medas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
